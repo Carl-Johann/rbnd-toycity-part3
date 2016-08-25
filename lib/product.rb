ee46ec913549aab57a682d0613969e32d3576a99
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-25 22:20:14 +0200

    in_stock? and in_stock methods made

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,12 @@
 require_relative 'errors'
 class Product 
-	attr_reader :title
-
+	attr_reader :title, :price, :stock
 	@@products = []
 	
 	def initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products
 	end
 
@@ -13,11 +14,31 @@ class Product
 		@@products
 	end
 
+	def self.in_stock
+		@@products.select! {|product| product.stock >= 1}
+	end
+
+	def in_stock?
+		if @stock == 0
+			return false
+		else
+			return true
+		end
+	end
+
+	def self.find_by_title(title)
+		@@products.each do |product|
+			if product.title == title
+				return product
+			end
+		end
+	end
+
 	private
 
 	def add_to_products
-    @@products.each do |item|
-        if item.title == @title
+    @@products.each do |product|
+        if product.title == @title
             raise DuplicateProductError, "'#{@title}' already exists'"
         else
             @@products << self
